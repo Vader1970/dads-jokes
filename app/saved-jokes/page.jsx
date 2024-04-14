4b4a615093bc5c49acf133bddd6d90e735442848
Vader1970 <daniel.wilkey@gmail.com>
2024-04-14 15:10:18 +1200

    Save joke and conditions

--- app/saved-jokes/page.jsx
+++ app/saved-jokes/page.jsx
@@ -1,9 +1,51 @@
-// Public route for page Saved Jokes
+// Importing the createClient function from the Supabase server utilities
+import { createClient } from "../utils/supabase/server";
 
+// Public route component for the Saved Jokes page
 export default async function SavedJokes() {
+  // Creating a Supabase client instance
+  const supabase = createClient();
+  // Fetching user data from the authenticated session
+  const { data: userData } = await supabase.auth.getUser();
+  // Initializing an empty array to store fetched jokes
+  let jokes = [];
+
+  // Checking if the user is authenticated
+  if (userData.user) {
+    // If authenticated, fetching jokes data from the 'joke' table
+    const { data: jokesData, error } = await supabase.from("joke").select("*");
+    // Handling any errors that occur during the fetching process
+    if (error) {
+      console.error("Error fetching jokes:", error);
+    }
+    // Assigning fetched jokes data to the 'jokes' array
+    jokes = jokesData;
+  }
+
+  // Setting the header text based on the user's authentication status and the presence of saved jokes
+  let header = "Saved Jokes";
+
+  if (!jokes.length) {
+    header = "Save some jokes to see your jokes";
+  }
+
+  if (!userData.user) {
+    header = "Login to save your favorite jokes";
+  }
+
+  // Rendering the Saved Jokes page layout
   return (
-    <main className='bg-gray-800 min-h-screen flex flex-col items-center justify-center text-center text-whitep-4'>
-      <h1 className='text-2xl font-bold mb-4'>Saved Jokes</h1>
+    <main className='bg-gray-800 min-h-screen flex flex-col items-center justify-center text-center text-white p-4'>
+      <h1 className='text-2xl font-bold mb-4'>{header}</h1>
+      <ul>
+        {/* Mapping through the 'jokes' array to render each saved joke */}
+        {jokes.map((joke) => (
+          <div key={joke.id} className='flex items-center justify-center space-x-2'>
+            <li className='list-none'>{joke.joke_text}</li>
+            {/* <DeleteJokeButton jokeId={joke.id} /> */}
+          </div>
+        ))}
+      </ul>
     </main>
   );
 }
